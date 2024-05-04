1991849449b1f14507843d7eace8f9d8042c2cea
Liam Nichols <liam.nichols.ln@gmail.com>
2024-05-04 11:14:52 +0200

    Update to xcstrings-tool-plugin 0.2.0

--- DogKit/Package.swift
+++ DogKit/Package.swift
@@ -13,7 +13,7 @@ let package = Package(
         )
     ],
     dependencies: [
-       .package(url: "https://github.com/liamnichols/xcstrings-tool-plugin", from: "0.1.2")
+       .package(url: "https://github.com/liamnichols/xcstrings-tool-plugin", from: "0.2.0")
     ],
     targets: [
         .target(
